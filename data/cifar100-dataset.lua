0a4bb6b08e0d167908eeb042056a72eac62f399a
yakir <yakir@presenso.com>
2017-02-05 20:57:13 +0200

    overall code arrangement

--- data/cifar100-dataset.lua
+++ data/cifar100-dataset.lua
@@ -4,72 +4,40 @@ require 'image'
 Dataset = {}
 local CIFAR, parent = torch.class("Dataset.CIFAR")
 
-function CIFAR:__init(path, mode, batchSize, zeroCodeFix, hadamardSize)
+function CIFAR:__init(path, mode, batchSize)
    local trsize = 50000
    local tesize = 5000 --10000
    self.batchSize = batchSize
    self.mode = mode
-   self.hadamardSize = hadamardSize
 
    if mode == "train" then
       self.data = torch.Tensor(trsize, 3*32*32)
       self.labels = torch.Tensor(trsize)
-      self.augLabels = torch.Tensor(100, self.hadamardSize):zero()
-      self.augLabelsFix = torch.Tensor(100, self.hadamardSize):zero()
       self.size = function() return trsize end
 
       local subset = torch.load(path..'/data_batch_1.t7', 'ascii')
       self.data[{ {1, 50000} }] = subset.data:t()
       self.labels[{ {1, 50000} }] = subset.labels
-      
-      if (self.hadamardSize == 63) then
-      	   self.augLabels[{ {1, 100} }] = subset.augLabels_63:t()
-      elseif (self.hadamardSize == 31) then
-           self.augLabels[{ {1, 100} }] = subset.augLabels_31:t()
-      elseif (self.hadamardSize == 127) then
-	   self.augLabels[{ {1, 100} }] = subset.augLabels_127:t()
-      elseif (self.hadamardSize == 255) then
-      	   self.augLabels[{ {1, 100} }] = subset.augLabels:t()
-      end
-      if zeroCodeFix == 0 then
-	      if (self.hadamardSize == 63) then
-        	   self.augLabelsFix[{ {1, 100} }] = subset.augLabelsFix_63:t()
-	      elseif (self.hadamardSize == 127) then
-                   self.augLabelsFix[{ {1, 100} }] = subset.augLabelsFix_31:t()
-	      elseif (self.hadamardSize == 127) then
-        	   self.augLabelsFix[{ {1, 100} }] = subset.augLabelsFix_127:t()
-	      elseif (self.hadamardSize == 255) then
-        	   self.augLabelsFix[{ {1, 100} }] = subset.augLabelsFix:t()
-      	      end
-      end
 
       self.labels = self.labels + 1
    elseif mode == "test" then
-      local subset = torch.load(path..'/test_batch.t7', 'ascii')
       self.data = torch.Tensor(tesize, 3*32*32)
       self.labels = torch.Tensor(tesize)
-
---      self.data = subset.data:t():double()
---      self.labels = subset.labels[1]:double()
       self.size = function() return tesize end
 
+	  local subset = torch.load(path..'/test_batch.t7', 'ascii')
       self.data[{ {1, 5000} }] = subset.data:t()[{ {5001, 10000} }]
       self.labels[{ {1, 5000} }] = subset.labels:t()[{ {5001, 10000} }]
 
-  
       self.labels = self.labels + 1
    elseif mode == "valid" then
-      local subset = torch.load(path..'/test_batch.t7', 'ascii')
       self.data = torch.Tensor(tesize, 3*32*32)
       self.labels = torch.Tensor(tesize)
-
---      self.data = subset.data:t():double()
---      self.labels = subset.labels[1]:double()
       self.size = function() return tesize end
 
+	  local subset = torch.load(path..'/test_batch.t7', 'ascii')
       self.data[{ {1, 5000} }] = subset.data:t()[{ {1, 5000} }]
       self.labels[{ {1, 5000} }] = subset.labels:t()[{ {1, 5000} }]
-
 
       self.labels = self.labels + 1
    end
@@ -90,56 +58,6 @@ function CIFAR:preprocess(mean, std)
    return mean,std
 end
 
-function CIFAR:updateAugLabelsFixes(labels, df_dw2, learnRate, outputs)  
-   local n = labels:size(1)
-
---   local counts = torch.Tensor(10):zero()
---  for i=1, n do
---	local idx = labels[i]
---	counts[idx] = counts[idx] + 1
---
---	if counts[idx] == 1 then
---		self.augLabelsFix:narrow(1,idx,1):zero()
---	end
-  -- end
-   for i=1, n do
-        local idx = labels[i]
-        local tmp1 = torch.Tensor(1,self.hadamardSize)
-        local tmp2 = torch.Tensor(1,self.hadamardSize)
-	local tmp3 = torch.Tensor(1,self.hadamardSize)
-        tmp1:copy(self.augLabelsFix:narrow(1,idx,1):float())
-        tmp2:copy(df_dw2:narrow(1,i,1):float())
-	tmp3:copy(outputs:narrow(1,i,1):float())
-        self.augLabelsFix:narrow(1,idx,1):copy(tmp1 - (learnRate/n) * tmp2)
---	self.augLabelsFix:narrow(1,idx,1):copy(tmp1 + (1/counts[idx]) * tmp3)
-
---	print("tmp2 = ")
----        print(tmp2)
- --       print("tmp3 = ")
- --       print(tmp3:float() - self.augLabels:narrow(1,idx,1):float())
-	
-
-   end
-end
-
-function CIFAR:SaveAugLabelsFixes(path)
-  local dataSet1 = torch.load(path..'/data_batch_1.t7', 'ascii')
-  --print('dataSet1.augLabelsFix size = ', dataSet1.augLabelsFix:size())
-  --print('self.augLabelsFix size = ', self.augLabelsFix:size())
-
-   if (self.hadamardSize == 63) then
-        dataSet1.augLabelsFix_63:copy(self.augLabelsFix:t())
-   elseif (self.hadamardSize == 31) then
-        dataSet1.augLabelsFix_31:copy(self.augLabelsFix:t())
-   elseif (self.hadamardSize == 127) then
-        dataSet1.augLabelsFix_127:copy(self.augLabelsFix:t())
-   elseif (self.hadamardSize == 255) then
-        dataSet1.augLabelsFix:copy(self.augLabelsFix:t())
-   end
-
-  torch.save(path..'/data_batch_1.t7', dataSet1, 'ascii')
-end
-
 function CIFAR:sampleIndices(batch, indices)
    if not indices then
       indices = batch
@@ -147,9 +65,7 @@ function CIFAR:sampleIndices(batch, indices)
    end
    local n = indices:size(1)
    batch = batch or {inputs = torch.zeros(n, 3, 32,32),
-                     outputs = torch.zeros(n),
-                     augLabels = torch.zeros(n, self.hadamardSize),
-                     augLabelsFix = torch.zeros(n, self.hadamardSize),
+                     outputs = torch.zeros(n)
                   }
    batch.outputs:copy(self.labels:index(1, indices))
    if self.mode == "train" then
